04193d16bbc490a8fc6fc98293922aefb68c38c2
cankush625 <cankush625@gmail.com>
2019-07-13 13:33:46 +0530

    Method to import class in another package is added

---
+++ intermediate/try_/tryForImport.java
@@ -0,0 +1,9 @@
+package try_;
+import class_.Song;
+
+public class tryForImport {
+	static Song song = new Song();
+	public static void main(String[] args) {
+		song.songDetails("ad", "2:34", "we", "df");
+	}
+}
